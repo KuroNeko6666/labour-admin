1afae132f97493e2c7a2d748a93515ed3007e513
“Agung <agungpriyatno666@email.com>
2022-09-06 14:26:33 +0700

    Update skill-edit.blade.php

--- resources/views/home/skill-edit.blade.php
+++ resources/views/home/skill-edit.blade.php
@@ -28,7 +28,7 @@
                     <div class="form-group row">
                         <label for="skill_group" class="col-sm-2 col-form-label">ID Kategori</label>
                         <div class="col-sm-10">
-                            <input name="skill_group" type="text" class="form-control @error('skill_group') is-invalid @enderror" id="skill_group" list="psycholog" value="{{ $data->category->id }}">
+                            <input name="skill_group" type="text" class="form-control @error('skill_group') is-invalid @enderror" id="skill_group" list="psycholog" value="{{ $data->skill_group }}">
                             <datalist id="psycholog">
                                 @foreach ($category as $item)
                                 <option value="{{ $item->id }}">{{ $item->category }}</option>
