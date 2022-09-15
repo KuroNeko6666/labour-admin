04486e3a55a6f4e735ceecf8520495a9810cb2a0
“Agung <agungpriyatno666@email.com>
2022-09-16 06:55:36 +0700

    Update vacancy-edit.blade.php

--- resources/views/home/vacancy-edit.blade.php
+++ resources/views/home/vacancy-edit.blade.php
@@ -111,7 +111,7 @@
                             @enderror
                         </div>
                         <div class="ml-3">
-                            <img id="output" height="200" width="200" src="/foto/{{ $data->foto }}">
+                            <img id="output" height="200" width="200" src="/gambar/{{ $data->gambar }}">
                             <p class="fs-6 text-center mt-2">Preview</p>
                         </div>
 
