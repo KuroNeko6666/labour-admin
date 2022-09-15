1414162a7eea7edd12b5e3d3f4bf65b6921af1a5
“Agung <agungpriyatno666@email.com>
2022-09-16 06:51:33 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -154,7 +154,7 @@ class UserController extends Controller
             $nameFile = 'FU-'.sprintf("%06s", $user->id);
             $nameFile .= '.'.$file->extension();
             $file->storeAs('foto', $nameFile);
-            $validated['foto'] = $nameFilea;
+            $validated['foto'] = $nameFile;
         } else {
             if ($request->email == $user->email){
                 $validated = $request->validate([
